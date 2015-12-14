93bb22c4b0995f73e711f75387998e0855ffc4fe
Mick Dudley <Mick@dudley.uk.com>
2015-12-14 16:06:54 +0000

    Deserialise finally!

--- src/test/java/restaurant/SerialiseTest.java
+++ src/test/java/restaurant/SerialiseTest.java
@@ -1,16 +1,30 @@
 package restaurant;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 public class SerialiseTest {
+
     @Before
     public void setup() {
 
     }
 
     @Test
-    public void testCanReadJson() {
+    public void testCanReadJson() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("order1.json").getFile());
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode jsonObj = mapper.readTree(file);
+        assertEquals(12, jsonObj.at("/tableNumber").asInt());
 
     }
 
