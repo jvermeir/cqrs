05dbd81b1ccb07303e91932bf26dd56f26b4ba9e
Mick Dudley <Mick@dudley.uk.com>
2015-12-14 15:22:57 +0000

    Add restaurant package

---
+++ src/test/java/restaurant/SerialiseTest.java
@@ -0,0 +1,18 @@
+package restaurant;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SerialiseTest {
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testCanReadJson() {
+
+    }
+
+
+}
